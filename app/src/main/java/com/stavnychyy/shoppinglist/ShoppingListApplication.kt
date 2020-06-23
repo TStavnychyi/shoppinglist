e08e492c5d6587d35f823390d5854d05aac38b61
Taras <taras.stavnychyi@empik.com>
2020-06-24 00:07:39 +0200

    Base project structure

---
+++ app/src/main/java/com/stavnychyy/shoppinglist/ShoppingListApplication.kt
@@ -0,0 +1,11 @@
+package com.stavnychyy.shoppinglist
+
+import android.app.Application
+
+
+class ShoppingListApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
